2ef53fc37e7b801118995993f41af94030b21f2e
yyj <yyjazsf@live.cn>
2016-11-20 21:51:22 +0800

    add 第一个 React 应用

---
+++ js/2.jsx
@@ -0,0 +1,11 @@
+/**
+ * 第一个 React 应用
+ */
+// http://www.zcfy.cc/article/building-your-first-react-app-1521.html
+var main = document.getElementById('main');
+
+ReactDOM.render(
+  <h1>yingyj.com</h1>,
+  main
+)
+
---
+++ js/3.jsx
@@ -0,0 +1,11 @@
+/**
+ * 组件
+ */
+// http://www.zcfy.cc/article/components-in-react-1524.html
+var main = document.getElementById('main');
+
+ReactDOM.render(
+  <h1>yingyj.com</h1>,
+  main
+)
+
