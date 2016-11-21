ba7378c641a09fc9c2304a24fae6534dd34cce9a
Ivan Ying <yyjazsf@live.cn>
2016-11-21 18:01:55 +0800

    完成 组件

--- js/3.jsx
+++ js/3.jsx
@@ -4,8 +4,34 @@
 // http://www.zcfy.cc/article/components-in-react-1524.html
 var main = document.getElementById('main');
 
+var HelloWorld = React.createClass({
+  render:function(){
+    console.log(this.props);
+    return (
+      <p>hello {this.props.greetTarget}</p>
+    );
+  }
+})
+
 ReactDOM.render(
-  <h1>yingyj.com</h1>,
+  <div>
+    <HelloWorld greetTarget="yyj" name="xxxx">
+      <div>
+      </div>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+        </ul>
+    </HelloWorld>
+    <HelloWorld greetTarget="xxg">
+      什么鬼
+    </HelloWorld>
+    <HelloWorld greetTarget="zsf"/>
+    <HelloWorld greetTarget="zry"/>
+    <HelloWorld greetTarget="aaa"/>
+    <HelloWorld greetTarget="bbb"/>
+    <HelloWorld greetTarget="ccc"/>
+  </div>,
   main
 )
 
